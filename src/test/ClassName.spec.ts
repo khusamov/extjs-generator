1366e5c6462a92e5ec9ba021c9d4f552edc98364
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-07 11:54:25 +0300

    Расширена спецификация Парсинг имени класса

--- src/test/ClassName.spec.ts
+++ src/test/ClassName.spec.ts
@@ -13,21 +13,33 @@ describe('ClassName', function() {
 		assert.isArray(className.path, 'Ожидается массив');
 		assert.equal(className.path.length, 0, 'Ожидается пустой массив');
 	});
-	it('Парсинг имени класса', function() {
-		const parsed: IClassName = ClassName.parse('Namespace.path1.path2.path3.ClassName');
-		assert.strictEqual<string>(parsed.name, 'ClassName');
-		assert.deepEqual<string[]>(parsed.path, ['path1', 'path2', 'path3']);
-		assert.strictEqual<string>(parsed.namespace as string, 'Namespace');
-	});
-	it('Имя класса не совпадает с пространством имен', function() {
-		const name = 'Namespace.path1.path2.path3.ClassName';
-		assert.throw(function() {
-			const parsed = ClassName.parse(name, 'Namespace2.path6');
+
+	describe('Парсинг имени класса', function() {
+		it('Парсинг имени класса без пространства имен', function() {
+			const parsed: IClassName = ClassName.parse('Namespace.path1.path2.path3.ClassName');
+			assert.strictEqual<string>(parsed.name, 'ClassName');
+			assert.deepEqual<string[]>(parsed.path, ['path1', 'path2', 'path3']);
+			assert.strictEqual<string>(parsed.namespace as string, 'Namespace');
 		});
-		assert.doesNotThrow(function() {
-			const parsed = ClassName.parse(name, 'Namespace.path1');
+		it('Парсинг имени класса с определением пространства имен', function() {
+			const namespace = 'Namespace.path1';
+			const parsed: IClassName = ClassName.parse(namespace + '.path2.path3.ClassName', namespace);
+			assert.strictEqual<string>(parsed.name, 'ClassName');
+			assert.deepEqual<string[]>(parsed.path, ['path2', 'path3']);
+			assert.strictEqual<string>(parsed.namespace as string, namespace);
+		});
+		it('Имя класса не совпадает с пространством имен', function() {
+			const name = 'Namespace.path1.path2.path3.ClassName';
+			assert.throw(function() {
+				const parsed = ClassName.parse(name, 'Namespace2.path6');
+			});
+			assert.doesNotThrow(function() {
+				const parsed = ClassName.parse(name, 'Namespace.path1');
+			});
 		});
 	});
+
+
 	it('Получение имени файла, соответствующего имени класса', function() {
 		const name = 'Namespace.path1.path2.path3.ClassName';
 		const filename = 'path1/path2/path3/ClassName.js';

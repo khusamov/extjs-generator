1ae06b09d3bf92bb8f995f5fca28445f3c12d381
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-07 15:50:06 +0300

    Тест класса Manager сделан более строгим

--- src/test/Manager.spec.ts
+++ src/test/Manager.spec.ts
@@ -5,9 +5,19 @@ import Manager from '../lib/Manager';
 import Namespace from '../lib/Namespace';
 
 describe('Manager', function() {
-	it('create', function() {
+	it('Пустой менеджер пространств имен', function() {
 		const manager = new Manager();
 		assert.ok<Manager>(manager);
+		assert.strictEqual<number>(manager.count, 0);
+	});
+	it('Получить несуществующее пространство имен', function() {
+		const manager = new Manager();
+		assert.isUndefined(manager.get('SampleNamespace1'));
+	});
+	it('Получить пространство имен по имени', function() {
+		const manager = new Manager();
+		const sampleNamespace1 = new Namespace('SampleNamespace1', manager);
+		assert.strictEqual<Namespace>(manager.get('SampleNamespace1'), sampleNamespace1);
 	});
 	it('Добавление двух пространств имен', function() {
 		const manager = new Manager();
@@ -16,22 +26,14 @@ describe('Manager', function() {
 		manager
 			.add(sampleNamespace1)
 			.add(sampleNamespace2);
-		assert.equal(manager.get('SampleNamespace1'), sampleNamespace1);
-		assert.equal(manager.get('SampleNamespace2'), sampleNamespace2);
-	});
-	it('get', function() {
-		const manager = new Manager();
-		const sampleNamespace1 = new Namespace('SampleNamespace1', manager);
-		assert.equal(manager.get('SampleNamespace1'), sampleNamespace1);
-	});
-	it('Получить несуществующее пространство имен', function() {
-		const manager = new Manager();
-		assert.isUndefined(manager.get('SampleNamespace1'));
+		assert.strictEqual<number>(manager.count, 2);
+		assert.strictEqual<Namespace>(manager.get('SampleNamespace1'), sampleNamespace1);
+		assert.strictEqual<Namespace>(manager.get('SampleNamespace2'), sampleNamespace2);
 	});
 	it('Добавление одинаковых пространств имен', function() {
 		const manager = new Manager();
 		const sampleNamespace1 = new Namespace('SampleNamespace1');
 		manager.add(sampleNamespace1).add(sampleNamespace1);
-		assert.equal(manager.count, 1, 'Было добавлено одно пространство имен');
+		assert.strictEqual<number>(manager.count, 1, 'Было добавлено одно пространство имен');
 	});
 });

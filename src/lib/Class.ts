f17d48d16de07dd2876e852df1664bb9ebf29c50
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-08 15:38:09 +0300

    В конструктор Class добавлена проверка валидности имени класса

--- src/lib/Class.ts
+++ src/lib/Class.ts
@@ -1,9 +1,11 @@
 import {ObjectNode} from 'khusamov-javascript-generator';
 import Namespace from './Namespace';
+import ClassName from './ClassName';
 
 export default class Class extends ObjectNode {
 	constructor(name: string, public namespace?: Namespace, public config?: any) {
 		super(name);
+		if (!ClassName.isValid(name)) throw new Error(`Имя класса '${name}' ошибочное.`);
 		if (namespace) namespace.add(this);
 	}
 }

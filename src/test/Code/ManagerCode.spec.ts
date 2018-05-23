dfd83b7817281adcf55250dc1286eca471e9f255
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-23 18:10:58 +0300

    Добавлен тест 'Запись класса в файл' для класса ManagerCode

---
+++ src/test/Code/ManagerCode.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+
+import * as Os from 'os';
+import * as Path from 'path';
+import * as Fs from "fs";
+import * as Util from "util";
+import * as Del from 'del';
+
+import { Ext, Code } from '../../index';
+
+const readFile = Util.promisify(Fs.readFile);
+
+describe('ManagerCode', function() {
+	it('Запись класса в файл', async function() {
+		const manager1 = new Ext.Manager();
+		const namespace1 = new Ext.Namespace('Namespace1', manager1);
+		const class1 = new Ext.Class('Namespace1.Class1', namespace1);
+		const managerCode1 = new Code.ManagerCode(manager1);
+		const targetDir = Path.join(Os.tmpdir(), '@ManagerCode_TempDir');
+
+		await managerCode1.saveTo(targetDir);
+
+		const class1fileContent = await readFile(Ext.ClassName.toSourceFileName(class1.name, targetDir), {encoding: 'utf8'});
+		assert.equal(class1fileContent, `Ext.define('Namespace1.Class1', {});\n`);
+
+		await Del(targetDir, {force: true});
+	});
+});

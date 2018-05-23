b82be9d795037267e1f77405a24cbf9bb879094a
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-23 18:08:03 +0300

    Исправлен тест Code.ClassCode

--- src/test/Code/ClassCode.spec.ts
+++ src/test/Code/ClassCode.spec.ts
@@ -1,27 +1,30 @@
 import { describe, it } from 'mocha';
 import { assert } from 'chai';
 import { normalizeString } from '../util';
-import { Ext, Code } from '../../index';
+import { Ext, Code, JavaScript } from '../../index';
 
-describe('Ext.code.ClassCode', function() {
-	it('Ext.code.ClassCode', function() {
+describe('Code.ClassCode', function() {
+	it('Code.ClassCode', function() {
+		// Внимание, здесь приходится создавать узел JavaScript.ExpressionNode, потому что
+		// функция alert для компилятора TypeScript не известна и компиляция не проходит.
 		const personClass = new Ext.Class('My.sample.Person', {
-			name: 'Unknown',
+			name: 'Unknown Person',
 			constructor: function(name) {
 				if (name) {
 					this.name = name;
 				}
 			},
-			eat: function(foodType) {
+			eat: new JavaScript.ExpressionNode(undefined, `function(foodType) {
 				alert(this.name + " is eating: " + foodType);
-			}
+			}`)
 		});
+
 		const personClassCode = new Code.ClassCode(personClass);
 		assert.strictEqual<string>(
 			normalizeString(personClassCode.toString()),
 			normalizeString(`
 				Ext.define('My.sample.Person', {
-					name: 'Unknown',
+					name: 'Unknown Person',
 					constructor: function(name) {
 						if (name) {
 							this.name = name;

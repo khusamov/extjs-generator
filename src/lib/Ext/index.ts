42aa6ba1faadc04e0b32e57ea49bdbebe97db0b3
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-28 18:13:06 +0300

    Добавлен экспорт классов Workspace и Package из модуля

--- src/lib/Ext/index.ts
+++ src/lib/Ext/index.ts
@@ -2,6 +2,8 @@ export { default as ClassName, IClassName } from './ClassName';
 export { default as Manager } from './Manager';
 export { default as Namespace } from './Namespace';
 export { default as Class } from './Class';
+export { default as Workspace } from './Workspace';
+export { default as Package } from './Package';
 
 import * as data from './data';
 export { data };

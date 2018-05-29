0fac9e630c0cf1fc36ef8fac87db12093d68f8f5
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-29 18:57:52 +0300

    В классе Workspace удален метод createPackage() и добавлены методы add() и save()

--- src/lib/Ext/Workspace.ts
+++ src/lib/Ext/Workspace.ts
@@ -7,10 +7,14 @@ import Package from './Package';
 const readFile = Util.promisify(Fs.readFile);
 
 /**
+ * Рабочее пространства ExtJS-проекта.
+ * Представляет из себя коллекцию пакетов проекта (Package[]).
+ * Предоставляет следующие функции:
  * Чтение и парсинг директории с рабочим пространством проекта на Sencha ExtJS.
  */
 export default class Workspace {
 	private config: object;
+	private packages: Package[];
 	public dir: string;
 	constructor() {}
 	async load(dir: string) {
@@ -18,7 +22,12 @@ export default class Workspace {
 		const configFilePath = Path.join(dir, 'workspace.json');
 		this.config = Json5.parse(await readFile(configFilePath, {encoding: 'utf8'}));
 	}
-	createPackage(name: string): Package {
-		return new Package(name, this);
+	add(pkg: Package): this {
+		pkg.workspace = this;
+		this.packages.push(pkg);
+		return this;
+	}
+	async save() {
+		await Promise.all(this.packages.map(pkg => pkg.save()));
 	}
 }

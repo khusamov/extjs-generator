f598c3e90ed68fa3744ec1c317af439e75274394
Khusamov Sukhrob <khusamov@yandex.ru>
2018-05-30 14:21:27 +0300

    В файл util добавлена функция getTargetDir()

--- src/test/util.ts
+++ src/test/util.ts
@@ -1,4 +1,13 @@
+import * as Path from "path";
+import * as Os from "os";
+
+export default {normalizeString, getTargetDir};
+
 export function normalizeString(str: string): string {
 	const EOL = '\n';
 	return str.trim().split(EOL).map(str => str.trim()).join(EOL);
+}
+
+export function getTargetDir(dirname): string {
+	return Path.join(Os.tmpdir(), `@${dirname}-${Math.random()}`);
 }

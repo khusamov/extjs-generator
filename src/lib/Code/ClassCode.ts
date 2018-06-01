a5b623756c95776f9cbe3a9fc379e0234274e824
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-01 17:33:31 +0300

    В классе ClassCode включена опция excludeEmptyNodes = true

--- src/lib/Code/ClassCode.ts
+++ src/lib/Code/ClassCode.ts
@@ -2,6 +2,7 @@ import { ObjectCode } from 'khusamov-javascript-generator';
 import Class from '../Ext/Class';
 
 export default class ClassCode extends ObjectCode<Class> {
+	protected excludeEmptyNodes = true;
 	protected createExpressionToString(): string {
 		return `Ext.define('${this.node.name}', ${this.nodeView.toString()})${this.suffix}`;
 	}

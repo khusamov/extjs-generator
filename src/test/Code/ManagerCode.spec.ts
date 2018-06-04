585146231a45622c716cd7ff0b89c9871afb9f34
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-04 19:00:01 +0300

    Доработан тест ManagerCode Запись класса в файл

--- src/test/Code/ManagerCode.spec.ts
+++ src/test/Code/ManagerCode.spec.ts
@@ -6,23 +6,26 @@ import { describe, it } from 'mocha';
 import { assert } from 'chai';
 
 import { getTargetDir } from '../util';
-import { Ext, Code } from '../../index';
+import {Ext, Code, JavaScript} from '../../index';
 
 const readFile = Util.promisify(Fs.readFile);
 
 describe('ManagerCode', function() {
 	it('Запись класса в файл', async function() {
+		const targetDir = getTargetDir('ManagerCodeTestDir');
+		// Создаем менеджер с кодом.
 		const manager1 = new Ext.Manager();
 		const namespace1 = new Ext.Namespace('Namespace1', manager1);
 		const class1 = new Ext.Class('Namespace1.Class1', namespace1);
+		// Создаем объект для преобразования содержимого менеджера в файлы с кодом классов.
 		const managerCode1 = new Code.ManagerCode(manager1);
-		const targetDir = getTargetDir('ManagerCodeTestDir');
-
+		// Сохраняем код во временную директорию.
 		await managerCode1.saveTo(targetDir);
-
-		const class1fileContent = await readFile(Ext.ClassName.toSourceFileName(class1.name, targetDir), {encoding: 'utf8'});
-		assert.equal(class1fileContent, `Ext.define('Namespace1.Class1', {});\n`);
-
+		// Проверяем сохраненный результат.
+		const class1FilePath = Ext.ClassName.toSourceFileName(class1.name, targetDir);
+		const class1FileContent = await readFile(class1FilePath, {encoding: 'utf8'});
+		assert.equal(class1FileContent, `Ext.define('Namespace1.Class1', {});\n`);
+		// Удаляем временную директорию.
 		await Del(targetDir, {force: true});
 	});
 });

34213dd6a8d53ef1852c37e1d4eca90a53ac06c0
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-04 19:30:03 +0300

    Добавление в тест 'Запись класса в файл' разделов before/after

--- src/test/Code/ManagerCode.spec.ts
+++ src/test/Code/ManagerCode.spec.ts
@@ -2,7 +2,7 @@ import * as Fs from "fs";
 import * as Util from "util";
 import * as Del from 'del';
 
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 import { assert } from 'chai';
 
 import { getTargetDir } from '../util';
@@ -11,24 +11,27 @@ import {Ext, Code, JavaScript} from '../../index';
 const readFile = Util.promisify(Fs.readFile);
 
 describe('ManagerCode', function() {
-	it('Запись класса в файл', async function() {
-		const targetDir = getTargetDir('ManagerCodeTestDir');
-		// Создаем менеджер с кодом.
-		const manager1 = new Ext.Manager();
-		const namespace1 = new Ext.Namespace('Namespace1', manager1);
-		const class1 = new Ext.Class('Namespace1.Class1', namespace1);
-		// Создаем объект для преобразования содержимого менеджера в файлы с кодом классов.
-		const managerCode1 = new Code.ManagerCode(manager1);
-		// Сохраняем код во временную директорию.
-		await managerCode1.saveTo(targetDir);
-		// Проверяем сохраненный результат.
-		const class1FilePath = Ext.ClassName.toSourceFileName(class1.name, targetDir);
-		const class1FileContent = await readFile(class1FilePath, {encoding: 'utf8'});
-		assert.equal(class1FileContent, `Ext.define('Namespace1.Class1', {});\n`);
-		// Удаляем временную директорию.
-		await Del(targetDir, {force: true});
+	describe('Запись класса в файл', function() {
+		let targetDir;
+		// Перед тестом генерируем путь к временной директории.
+		before(() => {targetDir = getTargetDir('ManagerCodeTestDir');});
+		// После теста удаляем временную директорию.
+		after(async () => await Del(targetDir, {force: true}));
+		it('Запись класса в файл', async function() {
+			// Создаем менеджер с кодом.
+			const manager1 = new Ext.Manager();
+			const namespace1 = new Ext.Namespace('Namespace1', manager1);
+			const class1 = new Ext.Class('Namespace1.Class1', namespace1);
+			// Создаем объект для преобразования содержимого менеджера в файлы с кодом классов.
+			const managerCode1 = new Code.ManagerCode(manager1);
+			// Сохраняем код во временную директорию.
+			await managerCode1.saveTo(targetDir);
+			// Проверяем сохраненный результат.
+			const class1FilePath = Ext.ClassName.toSourceFileName(class1.name, targetDir);
+			const class1FileContent = await readFile(class1FilePath, {encoding: 'utf8'});
+			assert.equal(class1FileContent, `Ext.define('Namespace1.Class1', {});\n`);
+		});
 	});
-
 	describe('Проверка, не перезаписывается ли Class.valueDefault', function() {
 		it('ObjectNode', function() {
 			const node1 = new JavaScript.ObjectNode('node1', {

99be7240c8f32f3c54284596f32aefcc42820557
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-05 19:23:18 +0300

    Переписан тест Создание пустого пакета

--- src/test/Ext/Package.spec.ts
+++ src/test/Ext/Package.spec.ts
@@ -4,7 +4,7 @@ import * as Fs from "fs";
 import * as Del from 'del';
 import MakeDir = require('mkdirp-promise');
 
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
 import { assert } from 'chai';
 
 import { getTargetDir } from '../util';
@@ -15,39 +15,91 @@ const writeFile = Util.promisify(Fs.writeFile);
 const stat = Util.promisify(Fs.stat);
 
 describe('Package', function() {
-	it('Создание пустого пакета', async function() {
-		// Создание временной директории фейкового рабочего пространства.
-		const workspaceDir = await createFakeWorkspace();
-		// Создание пустого пакета в фейковом рабочем пространстве.
-		const workspace1 = new Ext.Workspace;
-		await workspace1.load(workspaceDir);
-		const package1 = new Ext.Package('package1');
-		package1.manager = new Ext.Manager;
-		workspace1.add(package1);
-		await workspace1.save();
-		// Проверка, создан ли пакет или нет.
-		const package1DirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1'));
-		assert.isTrue(package1DirStat.isDirectory(), 'Путь к пакету должен существовать и быть директорией');
-		// Проверка, созданы ли директории пакета или нет.
-		const package1SourceDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.sourceDir));
-		const package1OverrideDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.overrideDir));
-		assert.isTrue(package1SourceDirStat.isDirectory(), 'Путь sourceDir должен существовать и быть директорией');
-		assert.isTrue(package1OverrideDirStat.isDirectory(), 'Путь overrideDir должен существовать и быть директорией');
-		// Проверка, созданы ли конфигурационные файлы пакета или нет.
-		const package1BuildXmlFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'build.xml');
-		const package1PackageJsonFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'package.json');
-		const package1BuildXmlFileStat = await stat(package1BuildXmlFilePath);
-		const package1PackageJsonFileStat = await stat(package1PackageJsonFilePath);
-		assert.isTrue(package1BuildXmlFileStat.isFile(), 'Файл build.xml должен существовать и быть директорией');
-		assert.isTrue(package1PackageJsonFileStat.isFile(), 'Файл package.json должен существовать и быть директорией');
-		// Проверка, содержат ли конфигурационные файлы имя пакета или нет.
-		const package1BuildXmlFile = await readFile(package1BuildXmlFilePath, {encoding: 'utf8'});
-		const package1PackageJsonFile = await readFile(package1PackageJsonFilePath, {encoding: 'utf8'});
-		assert.notStrictEqual<number>(package1BuildXmlFile.indexOf('package1'), -1);
-		assert.notStrictEqual<number>(package1PackageJsonFile.indexOf('package1'), -1);
-		// Удаление временной директории фейкового рабочего пространства.
-		await Del(workspaceDir, {force: true});
+
+	describe('Создание пустого пакета', function() {
+		let workspaceDir, package1;
+		before(async () => {
+			// Создание временной директории фейкового рабочего пространства.
+			workspaceDir = await createFakeWorkspace();
+			// Создание пустого пакета в фейковом рабочем пространстве.
+			const workspace1 = new Ext.Workspace;
+			await workspace1.load(workspaceDir);
+			package1 = new Ext.Package('package1');
+			package1.manager = new Ext.Manager;
+			workspace1.add(package1);
+			await workspace1.save();
+		});
+		after(async () => {
+			// Удаление временной директории фейкового рабочего пространства.
+			await Del(workspaceDir, {force: true});
+		});
+		describe('Структура пакета', function() {
+			it('Директория пакета должна быть создана', async function() {
+				const package1DirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1'));
+				assert.isTrue(package1DirStat.isDirectory(), 'Путь к пакету должен существовать и быть директорией');
+			});
+			it('Поддиректории пакета должны быть созданы', async function() {
+				const package1SourceDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.sourceDir));
+				const package1OverrideDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.overrideDir));
+				assert.isTrue(package1SourceDirStat.isDirectory(), 'Путь sourceDir должен существовать и быть директорией');
+				assert.isTrue(package1OverrideDirStat.isDirectory(), 'Путь overrideDir должен существовать и быть директорией');
+			});
+		});
+		describe('Конфигурационные файлы пакета', function() {
+			let package1BuildXmlFilePath, package1PackageJsonFilePath;
+			before(() => {
+				// Вычисление путей к конфигурационным файлам пакета.
+				package1BuildXmlFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'build.xml');
+				package1PackageJsonFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'package.json');
+			});
+			it('Конфигурационные файлы должны быть созданы', async function() {
+				const package1BuildXmlFileStat = await stat(package1BuildXmlFilePath);
+				const package1PackageJsonFileStat = await stat(package1PackageJsonFilePath);
+				assert.isTrue(package1BuildXmlFileStat.isFile(), 'Файл build.xml должен существовать и быть директорией');
+				assert.isTrue(package1PackageJsonFileStat.isFile(), 'Файл package.json должен существовать и быть директорией');
+			});
+			it('Конфигурационные файлы должны содержать имя пакета', async function() {
+				const package1BuildXmlFile = await readFile(package1BuildXmlFilePath, {encoding: 'utf8'});
+				const package1PackageJsonFile = await readFile(package1PackageJsonFilePath, {encoding: 'utf8'});
+				assert.notStrictEqual<number>(package1BuildXmlFile.indexOf('package1'), -1);
+				assert.notStrictEqual<number>(package1PackageJsonFile.indexOf('package1'), -1);
+			});
+		});
 	});
+
+	// it('Создание пустого пакета', async function() {
+	// 	// Создание временной директории фейкового рабочего пространства.
+	// 	const workspaceDir = await createFakeWorkspace();
+	// 	// Создание пустого пакета в фейковом рабочем пространстве.
+	// 	const workspace1 = new Ext.Workspace;
+	// 	await workspace1.load(workspaceDir);
+	// 	const package1 = new Ext.Package('package1');
+	// 	package1.manager = new Ext.Manager;
+	// 	workspace1.add(package1);
+	// 	await workspace1.save();
+	// 	// Проверка, создан ли пакет или нет.
+	// 	const package1DirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1'));
+	// 	assert.isTrue(package1DirStat.isDirectory(), 'Путь к пакету должен существовать и быть директорией');
+	// 	// Проверка, созданы ли директории пакета или нет.
+	// 	const package1SourceDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.sourceDir));
+	// 	const package1OverrideDirStat = await stat(Path.join(workspaceDir, 'packages/local', 'package1', package1.overrideDir));
+	// 	assert.isTrue(package1SourceDirStat.isDirectory(), 'Путь sourceDir должен существовать и быть директорией');
+	// 	assert.isTrue(package1OverrideDirStat.isDirectory(), 'Путь overrideDir должен существовать и быть директорией');
+	// 	// Проверка, созданы ли конфигурационные файлы пакета или нет.
+	// 	const package1BuildXmlFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'build.xml');
+	// 	const package1PackageJsonFilePath = Path.join(workspaceDir, 'packages/local', 'package1', 'package.json');
+	// 	const package1BuildXmlFileStat = await stat(package1BuildXmlFilePath);
+	// 	const package1PackageJsonFileStat = await stat(package1PackageJsonFilePath);
+	// 	assert.isTrue(package1BuildXmlFileStat.isFile(), 'Файл build.xml должен существовать и быть директорией');
+	// 	assert.isTrue(package1PackageJsonFileStat.isFile(), 'Файл package.json должен существовать и быть директорией');
+	// 	// Проверка, содержат ли конфигурационные файлы имя пакета или нет.
+	// 	const package1BuildXmlFile = await readFile(package1BuildXmlFilePath, {encoding: 'utf8'});
+	// 	const package1PackageJsonFile = await readFile(package1PackageJsonFilePath, {encoding: 'utf8'});
+	// 	assert.notStrictEqual<number>(package1BuildXmlFile.indexOf('package1'), -1);
+	// 	assert.notStrictEqual<number>(package1PackageJsonFile.indexOf('package1'), -1);
+	// 	// Удаление временной директории фейкового рабочего пространства.
+	// 	await Del(workspaceDir, {force: true});
+	// });
 	it('Создание пакета с двумя классами из одного пространства имен', async function() {
 		// Создание временной директории фейкового рабочего пространства.
 		const workspaceDir = await createFakeWorkspace();

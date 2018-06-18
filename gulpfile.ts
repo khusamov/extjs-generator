a02971188c8ef2a7f25d818e77af15c2b041e53e
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-18 10:11:41 +0300

    Gulpfile: Исправлена задача copyFiles

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,17 +1,32 @@
 import * as Path from 'path';
 import * as Gulp from 'gulp';
 import * as GulpChanged from 'gulp-changed';
-import * as GulpTypescript from 'gulp-typescript';
+import * as GulpTypeScript from 'gulp-typescript';
 
 /**
  * Gulpfile введен в разработку пока лишь с одной целью:
  * копировать JSON и XML файлы из src в dist.
  */
 
-const tsProject = GulpTypescript.createProject('tsconfig.json');
+const tsProject = GulpTypeScript.createProject('tsconfig.json');
 const tsConfig = require('./tsconfig.json');
 const tsConfigBaseUrl = tsConfig.compilerOptions.baseUrl;
 const tsConfigOutDir = tsConfig.compilerOptions.outDir;
+
+/**
+ * Копирование файлов JSON и XML из src в dist.
+ */
+Gulp.task('copyFiles', function() {
+	const src = (
+		['**/*.json', '**/*.xml']
+			.map(dir => Path.join(tsConfigBaseUrl, 'lib', dir))
+	);
+	return (
+		Gulp.src(src, {base: tsConfigBaseUrl})
+			.pipe(GulpChanged(tsConfigOutDir))
+			.pipe(Gulp.dest(tsConfigOutDir))
+	);
+});
 
 /**
  * Компиляция проекта в директорию dist.
@@ -24,18 +39,3 @@ Gulp.task('tsc', Gulp.parallel('copyFiles', function() {
 			.pipe(Gulp.dest(tsConfigOutDir))
 	);
 }));
-
-/**
- * Копирование файлов JSON и XML из src в dist.
- */
-Gulp.task('copyFiles', function() {
-	const src = (
-		['**/*.json', '**/*.xml']
-			.map(dir => Path.join(tsConfigBaseUrl, dir))
-	);
-	return (
-		Gulp.src(src)
-			.pipe(GulpChanged(tsConfigOutDir))
-			.pipe(Gulp.dest(tsConfigOutDir))
-	);
-});

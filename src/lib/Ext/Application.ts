c9d85acf099535a89192a42ecfee8a9988f64423
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-20 17:08:42 +0300

    Добавлены новые комментарии для класса Application

--- src/lib/Ext/Application.ts
+++ src/lib/Ext/Application.ts
@@ -3,6 +3,7 @@ import * as Path from 'path';
 import * as Util from "util";
 import * as Json5 from "json5";
 import Workspace from './Workspace';
+import Namespace from './Namespace';
 
 const readFile = Util.promisify(Fs.readFile);
 
@@ -12,23 +13,34 @@ const readFile = Util.promisify(Fs.readFile);
  * пока еще нет, но возможно в будущем будет создан).
  */
 export default class Application {
+	/**
+	 * Создание приложения.
+	 * Замена асинхронного конструктора.
+	 * @param {string} dir
+	 * @returns {Promise<Application>}
+	 */
 	static async load(dir: string): Promise<Application> {
-		const app = new this();
-		await app.load(dir);
-		app.dir = dir;
-		return app;
+		return await new this().load(dir);
 	}
+
 	private config: any;
 	private _workspace: Workspace;
+
+	/**
+	 * Директория приложения.
+	 * Абсолютный путь (сразу после вызова метода load()).
+	 * Путь, относительно корня рабочего пространства (после присоединения к рабочему пространству).
+	 */
 	dir: string;
+
 	get id(): string {
 		return this.config.id;
 	}
 	get name(): string {
 		return this.config.name;
 	}
-	get namespace(): string {
-		return this.config.namespace;
+	get namespace(): Namespace {
+		return new Namespace(this.config.namespace);
 	}
 	get requires(): string {
 		return this.config.requires;
@@ -40,8 +52,15 @@ export default class Application {
 		this._workspace = workspace;
 		this.dir = Path.normalize(this.dir).replace(Path.normalize(workspace.dir + '/'), '');
 	}
-	constructor() {}
-	async load(dir: string) {
+
+	/**
+	 * Загрузка данных приложения.
+	 * @param {string} dir Абсолютный путь к директории приложения.
+	 * @returns {Promise<this>}
+	 */
+	async load(dir: string): Promise<this> {
+		this.dir = dir;
 		this.config = Json5.parse(await readFile(Path.join(dir, 'app.json'), {encoding: 'utf8'}));
+		return this;
 	}
 }

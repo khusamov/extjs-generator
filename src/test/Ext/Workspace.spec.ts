2c5fbbbd182c93a4a04f920ca4fbcb0f40ea0086
Khusamov Sukhrob <khusamov@yandex.ru>
2018-06-26 12:34:37 +0300

    Добавлена генерация ошибки, если приложение из workspace.json не найдено

--- src/test/Ext/Workspace.spec.ts
+++ src/test/Ext/Workspace.spec.ts
@@ -1,25 +1,52 @@
+import * as Path from 'path';
 import * as Del from 'del';
 
 import { describe, it, before, after } from 'mocha';
 import { assert } from 'chai';
 
-import { getTargetDir, createFakeWorkspaceDir } from '../util';
+import { createFakeWorkspaceDir, createFakeErrWorkspaceDir } from '../util';
 import { Workspace } from '../../index';
 
 describe('Workspace', function() {
-	let workspaceDir: string;
-	before(async () => {
-		// Создание временной директории фейкового рабочего пространства.
-		workspaceDir = await createFakeWorkspaceDir();
+	describe('Рабочее пространство с одним приложением', function() {
+		let workspaceDir: string;
+		before(async () => {
+			// Создание временной директории фейкового рабочего пространства.
+			workspaceDir = await createFakeWorkspaceDir();
+		});
+		it('Чтение директорий приложений в рабочем пространстве', async function() {
+			const workspace = await Workspace.load(workspaceDir);
+			assert.strictEqual<number>(workspace.applications.length, 1);
+			assert.strictEqual<string>(workspace.applications[0].dir, 'pir-client');
+			assert.strictEqual<string>(workspace.applications[0].name, 'PirClient');
+		});
+		after(async () => {
+			// Удаление временной директории фейкового рабочего пространства.
+			await Del(workspaceDir, {force: true});
+		});
 	});
-	after(async () => {
-		// Удаление временной директории фейкового рабочего пространства.
-		await Del(workspaceDir, {force: true});
+	describe('Рабочее пространство с неправильной ссылкой на приложение', function() {
+		let workspaceDir: string;
+		before(async () => {
+			// Создание временной директории фейкового рабочего пространства.
+			workspaceDir = await createFakeErrWorkspaceDir();
+		});
+		it('Проверка генерации ошибки', async function() {
+			try {
+				await new Workspace().load(workspaceDir);
+			} catch (e) {
+				assert.throw(function() {
+					throw e;
+				}, [
+					`Приложение 'pir-client' не найдено.`,
+					`Исправьте файл '${Path.join(workspaceDir, 'workspace.json')}'.`
+				].join(' '));
+			}
+		});
+		after(async () => {
+			// Удаление временной директории фейкового рабочего пространства.
+			await Del(workspaceDir, {force: true});
+		});
 	});
-	it('Чтение директорий приложений в рабочем пространстве', async function() {
-		const workspace = await Workspace.load(workspaceDir);
-		assert.strictEqual<number>(workspace.applications.length, 1);
-		assert.strictEqual<string>(workspace.applications[0].dir, 'pir-client');
-		assert.strictEqual<string>(workspace.applications[0].name, 'PirClient');
-	});
+
 });
